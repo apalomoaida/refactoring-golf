5cdb2ae714ba12d01729b78e0c705df77fd06ab4
Angel Palomo Vallejo <apalomo@aidacanarias.com>
2022-05-03 18:04:43 +0200

    Hole 2:
    Para crear la clase money copie la pair borre el constuctor, el <A, b>, puse bien  los tips y genere su constructor con la bombilla 4(?), para aplicar la clase hice 3 replaces del cs. por tanto 7, en el video fueron 8 y eso que yo no uso resharper XD
    hole 2

---
+++ csharp/Hole2/Money.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Hole2
+{
+    public class Money
+    {
+        public readonly int value;
+        public readonly String currency;
+
+        public Money(int value, String currency) {
+            this.value = value;
+            this.currency = currency;
+        }
+    }
+}
